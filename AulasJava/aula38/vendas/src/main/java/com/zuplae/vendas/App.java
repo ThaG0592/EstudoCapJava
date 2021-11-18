01845950322ecc028c2bdfde2fc58e8a5d3a3030
Thais G. Gaiardo <thaisgaid@hotmail.com>
2021-11-18 10:09:14 -0300

    Aula Java 38

---
+++ AulasJava/aula38/vendas/src/main/java/com/zuplae/vendas/App.java
@@ -0,0 +1,16 @@
+package com.zuplae.vendas;
+
+import com.zuplae.vendas.view.CategoriaView;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        CategoriaView vc2 = new CategoriaView();
+        vc2.setDefaultCloseOperation(0);
+    }
+}
