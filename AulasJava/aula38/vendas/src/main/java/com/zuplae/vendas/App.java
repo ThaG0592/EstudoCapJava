940fd7e5d1b89e86f6c0d31bbf9d300669eb2c84
ThaG0592 <thaisgaid@hotmail.com>
2021-11-18 10:31:47 -0300

    atividade 38

--- AulasJava/aula38/vendas/src/main/java/com/zuplae/vendas/App.java
+++ AulasJava/aula38/vendas/src/main/java/com/zuplae/vendas/App.java
@@ -11,6 +11,6 @@ public class App
     public static void main( String[] args )
     {
         CategoriaView vc2 = new CategoriaView();
-        vc2.setDefaultCloseOperation(0);
+        vc2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
