ff33772ad50a79074570cb745ee06011e4895c21
ThaG0592 <thaisgaid@hotmail.com>
2021-11-19 11:04:23 -0300

    Aula40

---
+++ AulasJava/aula40/vendas3/src/main/java/com/vendas2/App.java
@@ -0,0 +1,13 @@
+package com.vendas2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
