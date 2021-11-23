fbc6bc1a3656c363e8a0e5136700c02e9ada387e
ThaG0592 <thaisgaid@hotmail.com>
2021-11-23 15:32:15 -0300

    ATP56

---
+++ Atividades/ATP56/atp056/src/main/java/com/form/atp056/models/Categoria.java
@@ -0,0 +1,27 @@
+package com.form.atp056.models;
+
+public class Categoria {
+    private int id;
+    private String nome;
+    private String descricao;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    public int getId() {
+        return id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    public String getDescricao() {
+        return descricao;
+    }
+}
