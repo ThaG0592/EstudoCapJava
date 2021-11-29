c53f2e53b340bb3fc2e8ae94505f4329a8cbcef3
ThaG0592 <thaisgaid@hotmail.com>
2021-11-29 13:55:20 -0300

    Criação das Classes ATP64

---
+++ Atividades/ATP64/SpringDATA/src/main/java/com/atp64/SpringDATA/model/SpringData.java
@@ -0,0 +1,39 @@
+package com.atp64.SpringDATA.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "SpringData")
+public class SpringData {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String tipo;
+    private String nome;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+    public String getTipo() {
+        return tipo;
+    }
+
+    
+}
