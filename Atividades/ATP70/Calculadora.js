15929402c0ca16eb005de68c60cc327b29620f14
ThaG0592 <thaisgaid@hotmail.com>
2021-12-01 22:18:48 -0300

    ATP70

---
+++ Atividades/ATP70/Calculadora.js
@@ -0,0 +1,11 @@
+export class Calculadora{
+    numero1 = 20;
+    numero2 = 70;
+
+}
+
+console.log(numero1+numero2);
+console.log(numero1-numero2);
+console.log(numero1*numero2);
+console.log(numero1/numero2);
+
---
+++ Atividades/ATP70/Cliente.js
@@ -0,0 +1,9 @@
+import { Pessoa } from './Pessoa';
+import {Calculadora} from './Calculadora';
+
+const pes1 = new Pessoa();
+pes1.nome = 'Zé';
+pes1.sobrenome = 'Grilo';
+pes1.idade = 90;
+
+console.log(pes1);
